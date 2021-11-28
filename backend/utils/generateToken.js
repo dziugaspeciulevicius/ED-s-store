ee79da79b65de4eeb000bc77b455956cd0b97bd3
dziugaspeciulevicius <dziugaspeciulevicius@gmail.com>
2021-11-28 15:02:48 +0200

    change jwt to expire in 2 hours

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,8 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "15min", //expires in 15 minutes
+    expiresIn: "2h", //expires in 15 minutes
+    // expiresIn: "15min", //expires in 15 minutes
   });
 };
 
